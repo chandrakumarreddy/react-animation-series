87d381cbc37757ff851d63136480b56d42cfe307
chandra655 <chandra.penugonda@unacademy.com>
2020-10-23 23:11:20 +0530

    add transiiton animation

--- animations/animation2/index.js
+++ animations/animation2/index.js
@@ -6,25 +6,53 @@ import {
   StatusBar,
   TouchableOpacity,
 } from 'react-native';
+import {Transition, Transitioning} from 'react-native-reanimated';
 
 import data from './data.json';
 
-export default function index() {
+const transition = (
+  <Transition.Together>
+    <Transition.In type="fade" durationMs={200} />
+    <Transition.Change />
+    <Transition.Out type="fade" durationMs={200} />
+  </Transition.Together>
+);
+
+export default function Animation2() {
+  const [currentIndex, setCurrentIndex] = React.useState(null);
+  const ref = React.useRef();
   return (
-    <View style={styles.container}>
+    <Transitioning.View
+      transition={transition}
+      style={styles.container}
+      ref={ref}>
       <StatusBar hidden />
-      {data.map(({bgColor, color, category}, index) => (
+      {data.map(({bgColor, color, category, subCategories}, index) => (
         <TouchableOpacity
           key={String(index)}
-          onPress={() => {}}
+          onPress={() => {
+            ref.current.animateNextTransition();
+            setCurrentIndex((prevIndex) =>
+              prevIndex === index ? null : index,
+            );
+          }}
           activeOpacity={0.9}
           style={[styles.cardContainer, {backgroundColor: bgColor}]}>
           <View style={[styles.card]}>
-            <Text>{category}</Text>
+            <Text style={[styles.heading, {color}]}>{category}</Text>
+            {currentIndex === index && (
+              <View style={styles.subCategory}>
+                {subCategories.map((_category, _index) => (
+                  <Text key={String(_index)} style={[{color, fontSize: 16}]}>
+                    {_category}
+                  </Text>
+                ))}
+              </View>
+            )}
           </View>
         </TouchableOpacity>
       ))}
-    </View>
+    </Transitioning.View>
   );
 }
 const styles = StyleSheet.create({
@@ -39,5 +67,15 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: '700',
+    lineHeight: 20 * 1.5,
+    letterSpacing: -2,
+    textTransform: 'uppercase',
+  },
+  subCategory: {
+    marginTop: 16,
   },
 });
